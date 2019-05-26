5e178660c244ff346c40580f71a8b2e01401a52a
daihyun <judaihyun1@gmail.com>
2019-05-26 22:21:18 +0900

    test chartjs with api

--- api/canvasChart.js
+++ api/canvasChart.js
@@ -13,13 +13,10 @@ canChart = () =>
         return -1;
     }
 
-    const chart = new Chart(ctx, {
+    let config = {
         type:'line',
         data:{
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
@@ -29,36 +26,114 @@ canChart = () =>
                     'rgba(255, 159, 64, 0.2)'
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'black'
                 ],
                 borderWidth: 1
             }]
         },
-    
-    });
+        options:{
+            title:{
+                display: true,
+                text: 'cpu usage'
+            },
+            legend:{
+                display:false
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        min: 0,
+                        max: 100
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: '%'
+                    }
+                }],
+                xAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'time'
+                    }
+                }]
+            },
+            hover:{
+                mode:'nearest',
+                intersect:true
+            }
+        }
+    }
+
+    const chart = new Chart(ctx);
+
+    return chart;
 }
+
+function reqListener ()
+{
+    let incomming = JSON.parse(this.responseText);
+    console.log(incomming);
+    addData(canChart, incomming.cpuUsage);
+};
+ 
+function addData() {
+    let xCount = 0;
+    return function(chart, data){
+
+        if(chart.data.labels.length > 5)
+        {
+            chart.data.datasets[0].data.shift();
+            chart.data.labels.shift();
+        }
+
+        chart.data.labels.push(xCount++);
+
+        chart.data.datasets.forEach((dataset) => {
+            dataset.data.push(data);
+        });
+
+        chart.update();
+    }
+};
+
+
 
 document.addEventListener('DOMContentLoaded',function(){
 
-var data = [30,86,120,43,1,2,4];
+    let chart = canChart();
+    let xAxesCount = 0;
+    let oReq = new XMLHttpRequest();
+    let addFunc = addData();
+    let intervalId;
 
-canChart();
+    
+    function request()
+    {
+        oReq.open("GET", "http://localhost:8080/cpuInfo");
+        oReq.send();
+    }
+
+    oReq.addEventListener("load", function(){
+        if(this.status === 200 && this.readyState === this.DONE)
+        {
+            let incomming = JSON.parse(this.responseText);
+            console.log(incomming.cpuUsage);
+            addFunc(chart, incomming.cpuUsage);
+        }else if(this.status === 500){
+            console.error('505 internal error');
+            return;
+        }else{
+            console.error('unknown error');
+        }
+    },false);
 
 
-function reqListener () {
-    let incomming = JSON.parse(this.responseText);
-    console.log(this.responseText);
-    console.log(incomming);
-  }
-  
-  var oReq = new XMLHttpRequest();
-  oReq.addEventListener("load", reqListener);
-  oReq.open("GET", "http://localhost:8080/cpuInfo");
-  oReq.send();
+    let startBtn = document.getElementById('start');
+    let stopBtn = document.getElementById('stop');
+
+    stopBtn.addEventListener('click', ()=>{clearInterval(intervalId);});
+    startBtn.addEventListener('click', ()=>{ intervalId = setInterval(request,2000);})
+
+    
 
 },false);
