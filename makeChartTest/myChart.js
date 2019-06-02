42c66096cc9d92ef603bf3be337643ba895b4734
daihyun <judaihyun1@gmail.com>
2019-06-02 20:10:20 +0900

    TODO : draw axes lable

--- makeChartTest/myChart.js
+++ makeChartTest/myChart.js
@@ -3,7 +3,9 @@ let config =
 {
     type:'line',
     data:{
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         datasets: [{
+            data:[1,2,-3,4],
             backgroundColor: [
             ],
             borderColor: [
@@ -12,7 +14,23 @@ let config =
         }]
     },
     options:{
-        responsive: false
+        responsive:true,
+        scales: {
+            xAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Month'
+                }
+            }],
+            yAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Value'
+                }
+            }]
+        } 
     } 
 };
 
@@ -38,6 +56,11 @@ let config =
         defaultLineHeight: 1.2
     };
 
+    let SIZE = {
+        inWidth : null,
+        inHeight : null,
+    }
+
     const defaultsOptions = 
     {
         elements:{},
@@ -60,15 +83,25 @@ let config =
         responsiveAnimationDuration: 0
     };
 
-    let helper = 
+    let Helper = 
     {
         baseCanvasImage : null,
-        isExist: function(value)
+        property : {},
+        isExist(value)
         {
             if(typeof value === 'undefined' && !value) return false;
             return true;
         },
-        contextValidator: function(item, config)
+        initConfig(config)
+        {
+            if(!Helper.isExist(config.options))
+            {
+                config.options = defaultsOptions;
+            }
+            
+            return config;
+        },
+        contextValidator(item, config)
         {
             if(!this.isExist(item)) return;
             if(!this.isExist(config)) return;
@@ -78,98 +111,157 @@ let config =
               //  console.log(items, config[item]);
             return item;
         },
-        saveDrawingSurface: function(ctx)
+        saveDrawingSurface(ctx)
         {
             this.baseCanvasImage = ctx.getImageData(0,0, ctx.canvas.width, ctx.canvas.height);
         },
-        resizingCanvas: function()
+        resizeForResponsive(ctx)
         {
-            
+            ctx.canvas.width = window.innerWidth,
+            ctx.canvas.height = (ctx.canvas.width / 21)*9;  // aspect ratio 21:9
+            //console.log('window.innerHeight : ' + window.innerHeight + ', window.innerWidth : ' + window.innerWidth);
+            //console.log('canvas.width : ' + ctx.canvas.width + ', canvas.height : ' + ctx.canvas.height);
+            initCanvas(ctx, config);
         }
-
     };
-
-    function initConfig(config)
-    {
-        if(!helper.isExist(config.options))
+    
+    let Draw = {
+        drawGrid(ctx)
         {
-            config.options = defaultsOptions;
-        }
-        return config;
-    };
+            ctx.save();
+            ctx.strokeStyle = 'gray';
+            ctx.lineWidth = 0.5;
 
-   
+            var width = ctx.canvas.width,
+                height = ctx.canvas.height;
+          
+            for(let i = 40.5; i < width - 40; i += 10)
+            {
+                ctx.beginPath();
+                ctx.moveTo(i, 0);
+                ctx.lineTo(i, height);
+                ctx.stroke();
+            }
+        
+            for(let i = 10.5; i < height; i += 10)
+            {
+                ctx.beginPath();
+                ctx.moveTo(40.5, i);
+                ctx.lineTo(width - 40, i);
+                ctx.stroke();
+            }
+
+            ctx.restore();
+        },
+        axesLable(ctx)
+        {
+            var width = ctx.canvas.width;
+            var height = ctx.canvas.height / 2;
+            var fontStyle = globalDefaults.defaultFontSize + 'px ' + 'Arial';
+
+            ctx.save();
+            ctx.fillStyle = 'red';
+            ctx.ineWidth = 1;
+            ctx.font = fontStyle;
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'middle';
+            ctx.translate(20, height);
+            ctx.rotate(Math.PI * 1.5);
+            ctx.fillText('test', 30, 0 );
+            ctx.restore();
+        }
+        
+    }
+
+    /*
+    scales: {
+            xAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Month'
+                }
+            }],
+            yAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Value'
+                }
+            }]
+        } 
+    */
 
     function initCanvas(ctx, config)
     {
-        ctx.save();
-        ctx.strokeStyle = 'black';
-        ctx.lineWidth = 0.5;
+        if(config.options.scales)
+        {
+            var scales = config.options.scales;
+        }
+        Draw.drawGrid(ctx);
+
+        for(var prop in scales)
+        {
+            console.log(prop);
+            Draw.axesLable(ctx, scales);
+        }
+
         
-        console.log('window.innerHeight : ' + window.innerHeight + ', window.innerWidth : ' + window.innerWidth);
-        console.log('canvas.width : ' + ctx.canvas.width + ', canvas.height : ' + ctx.canvas.height);
-        for(let i = 0.5; i < ctx.canvas.width; i += 10)
-        {
-            ctx.beginPath();
-            ctx.moveTo(i, 0);
-            ctx.lineTo(i, ctx.canvas.height);
-            ctx.stroke();
-        }
-
-        for(let i = 0.5; i < ctx.canvas.height; i += 10)
-        {
-            ctx.beginPath();
-            ctx.moveTo(0, i);
-            ctx.lineTo(ctx.canvas.width, i);
-            ctx.stroke();
-        }
-
-        ctx.restore();
     };
 
+   
     let JChart = function(item, config)
     {
-        this.construct(item, config);
-        this.bindEvent();
-        return this;
-    };
-
-    JChart.prototype.construct = function(item, config)
-    {
-        this.ctx = helper.contextValidator(item, config);
-        this.canvas = item.canvas;
-        this.config = initConfig(config);
-        this.initialize();
         this.resizeEvent = new Event('resizeEvt');
-        //update();
+        this.ctx = Helper.contextValidator(item, config);
+        this.canvas = item.canvas;
+        this.config = Helper.initConfig(config);
+        this.initialize();
+        return {
+            update : function()
+            {   
+            }
+        }
     };
 
     JChart.prototype.initialize = function()
     {
-        initCanvas(this.ctx, this.config);
-        helper.saveDrawingSurface(this.ctx);
-    };
+        var me = this;
+        initCanvas(me.ctx, me.config);
 
-    JChart.prototype.resizeCanvas = function(size)
-    {
-        console.log(`width : ${size.detail.inWidth}, height : ${size.detail.inHeight}`);
-        this.canvas.width = size.detail.inWidth;
-        this.canvas.height = size.detail.inHeight - 300;
-        initCanvas(this.ctx, this.config);
-    }
+        Helper.saveDrawingSurface(me.ctx);
+        me.bindEvent();
+        return me;
+    }; 
 
     JChart.prototype.bindEvent = function()
     {
-        document.addEventListener('resizeEvt', this.resizeCanvas.bind(this));
+        var me = this;
+        if(config.options.responsive)
+        {
+            Helper.resizeForResponsive(me.ctx);
+            this.bindResizeEvent();
+        }
+    }
 
+    JChart.prototype.resizeCanvas = function(size)
+    {
+        //console.log(`width : ${size.detail.inWidth}, height : ${size.detail.inHeight}`);
+        this.canvas.width = size.detail.inWidth;
+        this.canvas.height = (this.canvas.width / 21) * 9;
+        initCanvas(this.ctx, this.config);
+    }
+
+    JChart.prototype.bindResizeEvent = function()
+    {
+        document.addEventListener('resizeEvt', this.resizeCanvas.bind(this));
         window.addEventListener('resize',function(e){
-            var size = {
-                inWidth : e.target.innerWidth,
-                inHeight : e.target.innerHeight
-            }
-            document.dispatchEvent(new CustomEvent('resizeEvt', {detail:size}));
+            SIZE.inWidth = e.target.innerWidth;
+            SIZE.inHeight = e.target.innerHeight;
+            document.dispatchEvent(new CustomEvent('resizeEvt', {detail:SIZE}));
         },false);
     }
+  
 
     window.JChart = JChart;
 
@@ -182,7 +274,6 @@ document.addEventListener('DOMContentLoaded', function()
     let ctx = canvas.getContext('2d');
 
     let myChart = new JChart(ctx, config);
-
     //let chart = new Chart(ctx, config);
 
 },false);
