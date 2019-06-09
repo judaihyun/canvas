e9c4f1665d2d9b27ea08f6d09483767839e58acf
daihyun <judaihyun1@gmail.com>
2019-06-09 21:36:38 +0900

    TODO : To Make x rangeAxes

--- makeChartTest/myChart.js
+++ makeChartTest/myChart.js
@@ -3,12 +3,19 @@ let config =
 {
     type:'line',
     data:{
+        labels: [
+            'jan','feb'
+        ],
         datasets: [{
-            backgroundColor: [
-            ],
-            borderColor: [
-            ],
-            borderWidth: 1
+            label: 'firstLable',
+            data: [
+                5,1,4
+            ]
+        },{
+            label: 'secondLable',
+            data: [
+                2,3
+            ]
         }]
     },
     options:{
@@ -16,8 +23,8 @@ let config =
         layout: {
             padding: {
                 top: 40.5,
-				right: 0,
-				bottom: 40.5,
+				right: 20,
+				bottom: 30.5,
 				left: 65.5
             }
         },
@@ -42,7 +49,7 @@ let config =
 
 (function(global, factory)
 {
-    // UMD,  if exports, moudle object exists, then CommonJS
+    // UMD,  if module object exists, then CommonJS
     // if define object exists, then AMD
     // others global.useModuleName = factory()
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
@@ -123,7 +130,7 @@ let config =
             }
             //TODO make computedOptions
             computedOptions = Object.assign(computedOptions, config.options );
-          
+            
             return config;
         },
         contextValidator(item, config)
@@ -132,7 +139,7 @@ let config =
             if(!this.isExist(config)) return;
 
             // TODO  config validate
-            for(var items in config)
+            for(let items in config)
               //  console.log(items, config[item]);
             return item;
         },
@@ -161,10 +168,16 @@ let config =
                                                 (computedOptions.layout.padding.top +
                                                 computedOptions.layout.padding.bottom);
                 computedOptions.layout.bottomLable = {
-                    width : computedOptions.layout.chartWidth,
+                    width : computedOptions.layout.chartWidth + computedOptions.layout.padding.left,// + computedOptions.layout.padding.right,
                     height : computedOptions.layout.padding.top +
                             computedOptions.layout.chartHeight
+                };
+                /*
+                computedOptions.layout.xRangeAxis = {
+                    width : computedOptions.layout.chartWidth,
+                    height : computedOptions.
                 }
+                */
             
             }
             //console.log(ctx.canvas.height);
@@ -173,76 +186,124 @@ let config =
         },
         drawingRect(ctx) // for debug only
         {
-            ctx.save();
-            ctx.strokeStyle = 'red';
-            ctx.lineWidth = 0.5;
-            ctx.strokeRect(computedOptions.layout.padding.left,
+          
+            (function canvasArea(){
+                ctx.save();
+                ctx.strokeStyle = 'black';
+                ctx.lineWidth = 5;
+                ctx.strokeRect(0,
+                            0,
+                            ctx.canvas.width,
+                            ctx.canvas.height);
+                ctx.restore();
+            })();
+            
+            (function chartArea(){
+
+                ctx.save();
+                ctx.strokeStyle = 'red';
+                ctx.lineWidth = 0.5;
+                ctx.strokeRect(computedOptions.layout.padding.left,
                             computedOptions.layout.padding.top,
                             computedOptions.layout.chartWidth,
                             computedOptions.layout.chartHeight);
                             console.log('drawingRect height : ' + computedOptions.layout.chartHeight);
-            ctx.restore();
+                ctx.restore();
+            })();
+
+            (function xRangeAxis(){
+                ctx.save();
+                ctx.strokeStyle = 'green';
+                ctx.lineWidth = 0.5;
+                ctx.strokeRect(computedOptions.layout.padding.left,
+                            computedOptions.layout.padding.top,
+                            computedOptions.layout.chartWidth,
+                            computedOptions.layout.chartHeight);
+                ctx.restore();
+            })();
+
+          
+            (function bottomLable(){
+                ctx.save();
+                ctx.strokeStyle = 'blue';
+                ctx.lineWidth = 1;
+                ctx.strokeRect(0,
+                            computedOptions.layout.padding.top + computedOptions.layout.chartHeight,
+                            computedOptions.layout.bottomLable.width,
+                            computedOptions.layout.bottomLable.height);
+                ctx.restore();
+            })();
         }
     };
     
     let Draw = {
-        drawGrid()
+        drawGrid(data)
         {
-            var ctx = this.getContext();
+            console.log(data);
+            let ctx = this.getContext();
             ctx.strokeStyle = 'gray';
             ctx.lineWidth = 0.5;
 
-            var fontSize = globalDefaults.defaultFontSize,
+            let fontSize = globalDefaults.defaultFontSize,
             fontStyle = fontSize + 'px ' + 'Arial';
             ctx.font = fontStyle,
             ctx.textAlign = 'center',
             ctx.textBaseline = 'middle';
             
 
-            var width = ctx.canvas.width,
+            let width = ctx.canvas.width,
                 height = ctx.canvas.height;
             
             
-            var leftPadding = computedOptions.layout.padding.left,
+            let leftPadding = computedOptions.layout.padding.left,
+                rightPadding = computedOptions.layout.padding.right,
                 topPadding = computedOptions.layout.padding.top,
                 bottomPadding = computedOptions.layout.padding.bottom,
                 chartWidth = computedOptions.layout.chartWidth,
                 chartHeight = computedOptions.layout.chartHeight,
                 stepx = Math.ceil((chartHeight) / 11);  // 올림
-            var dummy = 0;
-            var stepy = chartWidth / 2;
+            let dummy = 0;
+
+        
+            let label = data.labels;
+            let dataSize = data.datasets[0].data.length - 1;
+            let stepy = chartWidth / dataSize;
 
             console.log('stepx : ' + stepx);
             console.log('stepy : ' + stepy);
-            console.log(chartHeight);
+            console.log(chartHeight + topPadding);
 
             ctx.save();
             // y
+           
             for(let i = leftPadding; i < chartWidth; i += stepy)
             {
                 ctx.beginPath();
                 ctx.moveTo(i, topPadding);
-                ctx.lineTo(i, chartHeight);
+                ctx.lineTo(i, chartHeight + 20.5);
+                console.log(label[i]);
+                ctx.fillText(label[i], i, chartHeight + bottomPadding);
                 ctx.stroke();
             }
+            
 
-            var text = 1.0;
+            let text = 1.0;
         
             // x
             var count = 0;
-            for(let i = topPadding; i < chartHeight + topPadding; i += stepx)
+            for(let x = topPadding; x < chartHeight + topPadding; x += stepx)
             {
                 ctx.beginPath();
                 ctx.lineWidth = 0.5;
-                console.log('i : ' + i);
                 if(count === 5)
                 {
                     ctx.lineWidth = 1;
                 }
                 text = text.toFixed(1);
-                ctx.fillText(text, leftPadding - 20, i);
-                ctx.moveTo(leftPadding - 10, i);
-                ctx.lineTo(width - 40, i);
+                ctx.fillText(text, leftPadding - 20, x);
+
+                ctx.moveTo(leftPadding - 10, x);
+                ctx.lineTo(leftPadding + chartWidth, x);
                 text -= 0.2;
                 count++;
                 ctx.stroke();
@@ -254,7 +315,7 @@ let config =
             Helper.drawingRect(ctx);
 
         },
-        axesLable(scales, axesType)
+        legendLable(scales, axesType)
         {
 
             var labelString,
@@ -273,12 +334,12 @@ let config =
             ctx.textAlign = 'center';
             ctx.textBaseline = 'middle';
 
-            if(axesType==='xAxes')
+            if(axesType==='xAxes') // bottom legend lable
             {
                 labelString = scales[0].scaleLabel.labelString;
                 width = ctx.canvas.width / 2;
                 ctx.fillText(labelString, width, computedOptions.layout.bottomLable.height + fontSize + 1);
-            }else if(axesType==='yAxes'){
+            }else if(axesType==='yAxes'){ // left legend lable
                 labelString = scales[0].scaleLabel.labelString;
                 width = ctx.canvas.width;
                 height = ctx.canvas.height / 2;
@@ -289,17 +350,28 @@ let config =
 
             ctx.restore();
         },
+        rangeAxis()
+        {
+            let ctx = this.getContext();
+
+
+        },
         baseCanvas(config)
         {
-            var opts = config.options;
+            let opts = config.options;
+            let data = config.data; 
+            Draw.drawGrid(data);
+            /*
+            datasets.forEach((datas) => {
+                Draw.drawGrid(datas);
+            })
+            */
 
-            Draw.drawGrid();
-
-            for(var axes in opts.scales)
+            for(let axes in opts.scales)
             {   
                 if(opts.scales[axes][0].display)
                 {
-                    Draw.axesLable(opts.scales[axes], axes);
+                    Draw.legendLable(opts.scales[axes], axes);
                 }
             }
         },
@@ -314,14 +386,16 @@ let config =
    
     let JChart = function(ctx, config)
     {
-        this.resizeEvent = new Event('resizeEvt');
-        this.ctx = Helper.contextValidator(ctx, config);
-        this.canvas = ctx.canvas;
-        this.config = Helper.initConfig(ctx, config);
-        this.initialize();
+        let me = this;
+        me.resizeEvent = new Event('resizeEvt');
+        me.ctx = Helper.contextValidator(ctx, config);
+        me.canvas = ctx.canvas;
+        me.config = Helper.initConfig(ctx, config);
+        me.initialize();
         return {
             update : function()
             {   
+               // Draw.baseCanvas(me.config);
             },
             values : function(){
                 return computedOptions;
@@ -331,7 +405,7 @@ let config =
 
     JChart.prototype.initialize = function()
     {
-        var me = this;
+        let me = this;
         Draw.setContext(me.ctx);
         
         Helper.computeSize(me.ctx);
@@ -343,7 +417,7 @@ let config =
 
     JChart.prototype.bindEvent = function()
     {
-        var me = this;
+        let me = this;
         if(config.options.responsive)
         {
             Helper.resizeForResponsive(me.ctx);
@@ -354,7 +428,7 @@ let config =
     JChart.prototype.resizeCanvas = function(size)
     {
         //console.log(`width : ${size.detail.inWidth}, height : ${size.detail.inHeight}`);
-        var me = this;
+        let me = this;
         me.canvas.width = size.detail.inWidth;
         me.canvas.height = (me.canvas.width / 21) * 9;  // aspect ratio 21:9
         Helper.computeSize(me.ctx);
@@ -384,7 +458,7 @@ document.addEventListener('DOMContentLoaded', function()
     
     let myChart = new JChart(ctx, config);
     //let chart = new Chart(ctx, config);
-    var value = myChart.values();
+    let value = myChart.values();
   
     document.getElementById('chartWidth').innerHTML = 'chartWidth : ' + value.layout.chartWidth;
     document.getElementById('chartHeight').innerHTML = 'chartHeight : ' + value.layout.chartHeight;
@@ -392,6 +466,10 @@ document.addEventListener('DOMContentLoaded', function()
     document.getElementById('bottomPadding').innerHTML = 'bottomPadding : ' + value.layout.padding.bottom;
     document.getElementById('leftPadding').innerHTML = 'leftPadding : ' + value.layout.padding.left;
     document.getElementById('rightPadding').innerHTML = 'rightPadding : ' + value.layout.padding.right;
+
+    document.getElementById('chartUpdate').addEventListener('click', function(){
+        myChart.update();
+    },false);
 
 
 },false);
